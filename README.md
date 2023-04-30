f6c5303c4c0887294b9180495ce4111c2022a9aa
Antoine <antoine1.anthony@gmail.com>
2023-04-30 16:31:19 -0700

    feat: add timing logs, saving responses from the PDF summary API locally, and updating the README

--- README.md
+++ README.md
@@ -1,58 +1,63 @@
-PDF Uploader from Directory README
-==================================
+# PDF Uploader with Timing and Response Saving README
 
-This is a Node.js script that uploads all PDF files in a specified directory to the PDF summary endpoint. After successful upload, the endpoint processes the PDF files and returns the results. This script is a helpful utility to automate the process of uploading and summarizing PDF files from a directory.
+This is a Node.js script that uploads all PDF files in a specified directory to the PDF summary endpoint, measures the time taken for processing, and saves the response data as separate files for each field in the response. This script is a helpful utility to automate the process of uploading, summarizing, and saving the results of multiple PDF files from a directory.
 
-Prerequisites
--------------
+## Prerequisites
 
 Before using this script, ensure that you have the following software installed on your machine:
 
-1.  Node.js (version 14 or higher)
+1\. Node.js (version 14 or higher)
 
-Installation
-------------
+## Installation
 
 To use this script, follow these steps:
 
-1.  Download or clone the repository containing the script.
-2.  Open a terminal and navigate to the directory containing the script.
-3.  Run `npm install` to install the required dependencies (axios, form-data, ora, and path).
+1\. Download or clone the repository containing the script.
 
-Usage
------
+2\. Open a terminal and navigate to the directory containing the script.
+
+3\. Run `npm install` to install the required dependencies (axios, form-data, ora, and path).
+
+## Usage
 
 To run the script, use the following command:
 
-phpCopy code
+```
 
-`node <script_filename> `
+node <script_filename>
 
-Replace `<script_filename>` with the name of the script file (e.g., `pdf_uploader_from_directory.js`).
+```
+
+Replace `<script_filename>` with the name of the script file (e.g., `pdf_uploader_with_timing.js`).
 
 By default, the script looks for PDF files in a directory named `PDFs`. If you want to change the directory, modify the value of the `pdfDirectory` variable in the script.
 
 For example, if you want to use a directory named `my_pdfs`, update the script as follows:
 
-javascriptCopy code
+```javascript
 
-`// Directory containing the PDF files
-const pdfDirectory = 'my_pdfs';`
+// Directory containing the PDF files
 
-Output
-------
+const pdfDirectory = 'my_pdfs';
 
-The script displays a spinner while the PDF files are being uploaded. Once the upload and processing are complete, it prints the results to the console.
+```
 
-If the files are processed successfully, you'll see a "Success: PDFs processed successfully" message, along with the response data from the endpoint. If there's an error during the upload or processing, an appropriate error message will be displayed.
+## Output
+
+The script measures the total time taken by the script, as well as the time taken for processing each response. It displays a spinner while the PDF files are being uploaded.
+
+Once the upload and processing are complete, the script saves the response data in a separate directory for each PDF file, located under a parent directory named `ResponseData`. The response data fields are saved as separate `.txt` files.
+
+If the files are processed successfully, you'll see a "Success: PDF API processed successfully" message, along with the paths to the saved response data files. If there's an error during the upload or processing, an appropriate error message will be displayed.
 
 If there are no PDF files in the specified directory, the script will display an "Error: No PDF files found in the specified directory" message.
 
-Notes
------
+## Notes
 
 This script assumes that the PDF summary endpoint is located at `http://127.0.0.1:8000/pdfsummary`. If the endpoint is hosted on a different address, you should update the URL in the `axios.post` function call.
 
 This script only supports PDF files. If the specified directory contains non-PDF files, they will be ignored.
 
 If you need to add additional headers, such as authentication tokens, you can modify the `headers` object in the `axios.post` function call.
+
+You can customize the response fields to save by modifying the `fields` array in the script. Currently, it saves the following fields: 'summary', 'notes', 'notes_summary', 'essential_info', and 'blog_post'.
--- pdfUploader.js
+++ pdfUploader.js
@@ -1,28 +1,26 @@
-// Description: Uploads PDF files to the PDF summary endpoint (node script)
+// Description: Uploads a single PDF file to the PDF summary endpoint (node script)
 import axios from 'axios';
 import FormData from 'form-data';
 import fs from 'fs';
 import path from 'path';
+import ora from 'ora';
 
-async function uploadPDFs(pdfFilePaths) {
+async function uploadPDF(pdfFilePath) {
   try {
     // Create a FormData object
     const formData = new FormData();
 
-    console.log('pdfFilePaths: ', pdfFilePaths);
+    // Append the PDF file to the FormData object (use the key 'pdfs')
+    const pdfData = fs.readFileSync(pdfFilePath);
+    formData.append('pdfs', pdfData, path.basename(pdfFilePath));
 
-    // Append each PDF file to the FormData object (use the key 'pdfs')
-    pdfFilePaths.forEach((pdfFilePath, index) => {
-      const pdfData = fs.readFileSync(pdfFilePath);
-      formData.append('pdfs', pdfData, `uploaded_pdf_${index}.pdf`);
-    });
+    // Create a spinner instance
+    const spinner = ora('Uploading PDF...').start(); // Start the spinner with a message
 
-    const ora = (await import('ora')).default;
+    // Start the response timer
+    console.time('Response Time');
 
-    // Create a spinner instance and start it
-    const spinner = ora('Uploading PDFs...').start();
-
-    // Send the PDF files to the endpoint
+    // Send the PDF file to the endpoint
     const response = await axios.post(
       'http://127.0.0.1:8000/pdfsummary',
       formData,
@@ -31,21 +29,53 @@ async function uploadPDFs(pdfFilePaths) {
       },
     );
 
+    // Stop the response timer
+    console.timeEnd('Response Time');
+
     // Stop the spinner
     spinner.stop();
 
     // Handle the response from the endpoint
     if (response.status === 200) {
       const responseData = response.data;
-      console.log('responseData: ', responseData);
-      console.log('Success: PDFs processed successfully');
+      console.log('Success: PDF API processed successfully');
+
+      // Create a directory with the name of the PDF file to save the response data
+      const responseDirectory = path.join(
+        'ResponseData',
+        path.basename(pdfFilePath, '.pdf'),
+      );
+      fs.mkdirSync(responseDirectory, { recursive: true });
+      console.log('responseDirectory: ', responseDirectory);
+
+      // Save each field of the response data as a separate file in the created directory
+      const fields = [
+        'summary',
+        'notes',
+        'notes_summary',
+        'essential_info',
+        'blog_post',
+      ];
+      fields.forEach((field) => {
+        if (responseData[field]) {
+          const filePath = path.join(responseDirectory, `${field}.txt`);
+          console.log('filePath: ', filePath);
+          fs.writeFileSync(filePath, responseData[field]);
+          console.log(`Success: PDF ${field}.txt file processed successfully`);
+        }
+      });
     } else {
-      throw new Error('Error: Failed to process PDFs');
+      throw new Error('Error: Failed to process PDF');
     }
   } catch (error) {
+    // Stop the spinner if there is an error
+    spinner.stop();
     console.error('Error:', error);
   }
 }
+
+// Start the script timer
+console.time('Script Time');
 
 // Directory containing the PDF files
 const pdfDirectory = 'PDFs';
@@ -54,11 +84,24 @@ const pdfDirectory = 'PDFs';
 const files = fs.readdirSync(pdfDirectory);
 
 // Filter the files to include only PDF files
-const pdfFilePaths = files.filter(file => path.extname(file).toLowerCase() === '.pdf')
-  .map(file => path.join(pdfDirectory, file));
+const pdfFilePaths = files
+  .filter((file) => path.extname(file).toLowerCase() === '.pdf')
+  .map((file) => path.join(pdfDirectory, file));
 
 if (pdfFilePaths.length > 0) {
-  uploadPDFs(pdfFilePaths);
+  // Iterate over each PDF file path and call the uploadPDF function for each file
+  Promise.all(pdfFilePaths.map(uploadPDF))
+    .then(() => {
+      // Stop the script timer after all PDFs have been processed
+      console.timeEnd('Script Time');
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // Stop the script timer in case of error
+      console.timeEnd('Script Time');
+    });
 } else {
   console.error('Error: No PDF files found in the specified directory');
+  // Stop the script timer if no PDF files are found
+  console.timeEnd('Script Time');
 }
